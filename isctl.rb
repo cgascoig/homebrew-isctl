d029bd65eb982b1cafc3adfb518f22ce59577047
Chris Gascoigne <cgascoig@cisco.com>
2024-04-08 23:08:55 +0000

    Brew formula update for isctl v0.2.1

--- isctl.rb
+++ isctl.rb
@@ -5,21 +5,21 @@
 class Isctl < Formula
   desc ""
   homepage ""
-  version "0.1.27"
+  version "0.2.1"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/cgascoig/isctl/releases/download/v0.1.27/isctl_0.1.27_Darwin_x86_64.zip"
-      sha256 "744177f39ee66303259803cddcc6e49db394cdd96b0c81d2e8a9cb6590c79ef3"
+      url "https://github.com/cgascoig/isctl/releases/download/v0.2.1/isctl_0.2.1_Darwin_x86_64.zip"
+      sha256 "fc4d1b3b093c3d27d4c2e7d610d1115e1971f7fa5e70709a3925194d6bfe135a"
 
       def install
         bin.install "isctl"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/cgascoig/isctl/releases/download/v0.1.27/isctl_0.1.27_Darwin_arm64.zip"
-      sha256 "d8636eff29a6452472609bde835e29c99b5b0794a0456950b5d5694ef58ee5c0"
+      url "https://github.com/cgascoig/isctl/releases/download/v0.2.1/isctl_0.2.1_Darwin_arm64.zip"
+      sha256 "eff17f00a39d3663d117ccd5e1ff3d4fabfa39856d26aacbb10be81a1cc63800"
 
       def install
         bin.install "isctl"
